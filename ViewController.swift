80619ffb842468bd437c23cb3edaa4236bf2edde
Simran <sam.sandhu131@gmail.com>
2020-02-27 12:35:47 +0530

    Setting Up API key and URL from we are going to get JSON data.

--- ViewController.swift
+++ ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Can create your own API Key from "calendarific.com"
+    var apiKey = "533b84a223a70aa1599d38660e5103628c842b3d"
+    var countryCode = "IN"
+    var holidayYear = "2020"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getHolidayData()
+        
     }
 
+    private func getHolidayData() {
+        
+        // this is will JSON data.
+        let modifiedURL = " https://calendarific.com/api/v2/holidays?&api_key=\(apiKey)&country=\(countryCode)&year=\(holidayYear)"
+        
+        
+        
+    }
 
 }
 
