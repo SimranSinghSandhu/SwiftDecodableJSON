3d0cba7f0be9a95735c1626b2e3133148365c07b
Simran <sam.sandhu131@gmail.com>
2020-02-27 12:29:28 +0530

    First Commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftDecodableJSON
+//
+//  Created by Simran Singh Sandhu on 27/02/20.
+//  Copyright © 2020 Simran Singh Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
